b650eee14f54de3ea9622e1da07729f6748c02d7
Bhanu <bhanu@crackviral.com>
2025-08-23 02:28:07 +0530

    bumped version and type lint fixes

--- src/core/edges.d.ts
+++ src/core/edges.d.ts
@@ -1,48 +1,38 @@
 import { z } from 'zod';
 import { BaseEdge, EntityEdge, EpisodicEdge, CommunityEdge, GraphDriver } from '../types/index.js';
-export declare const BaseEdgeSchema: z.ZodObject<
-  {
+export declare const BaseEdgeSchema: z.ZodObject<{
     uuid: z.ZodDefault<z.ZodString>;
     groupId: z.ZodString;
     sourceNodeUuid: z.ZodString;
     targetNodeUuid: z.ZodString;
     createdAt: z.ZodDefault<z.ZodDate>;
-  },
-  'strip',
-  z.ZodTypeAny,
-  {
+}, "strip", z.ZodTypeAny, {
     uuid: string;
     groupId: string;
     createdAt: Date;
     sourceNodeUuid: string;
     targetNodeUuid: string;
-  },
-  {
+}, {
     groupId: string;
     sourceNodeUuid: string;
     targetNodeUuid: string;
     uuid?: string | undefined;
     createdAt?: Date | undefined;
-  }
->;
-export declare const EntityEdgeSchema: z.ZodObject<
-  {
+}>;
+export declare const EntityEdgeSchema: z.ZodObject<{
     uuid: z.ZodDefault<z.ZodString>;
     groupId: z.ZodString;
     sourceNodeUuid: z.ZodString;
     targetNodeUuid: z.ZodString;
     createdAt: z.ZodDefault<z.ZodDate>;
-  } & {
+} & {
     name: z.ZodString;
-    factIds: z.ZodArray<z.ZodString, 'many'>;
-    episodes: z.ZodArray<z.ZodString, 'many'>;
+    factIds: z.ZodArray<z.ZodString, "many">;
+    episodes: z.ZodArray<z.ZodString, "many">;
     expiredAt: z.ZodOptional<z.ZodDate>;
     validAt: z.ZodDate;
     invalidAt: z.ZodOptional<z.ZodDate>;
-  },
-  'strip',
-  z.ZodTypeAny,
-  {
+}, "strip", z.ZodTypeAny, {
     uuid: string;
     name: string;
     groupId: string;
@@ -54,8 +44,7 @@ export declare const EntityEdgeSchema: z.ZodObject<
     episodes: string[];
     invalidAt?: Date | undefined;
     expiredAt?: Date | undefined;
-  },
-  {
+}, {
     name: string;
     groupId: string;
     factIds: string[];
@@ -67,48 +56,37 @@ export declare const EntityEdgeSchema: z.ZodObject<
     createdAt?: Date | undefined;
     invalidAt?: Date | undefined;
     expiredAt?: Date | undefined;
-  }
->;
-export declare const EpisodicEdgeSchema: z.ZodObject<
-  {
+}>;
+export declare const EpisodicEdgeSchema: z.ZodObject<{
     uuid: z.ZodDefault<z.ZodString>;
     groupId: z.ZodString;
     sourceNodeUuid: z.ZodString;
     targetNodeUuid: z.ZodString;
     createdAt: z.ZodDefault<z.ZodDate>;
-  },
-  'strip',
-  z.ZodTypeAny,
-  {
+}, "strip", z.ZodTypeAny, {
     uuid: string;
     groupId: string;
     createdAt: Date;
     sourceNodeUuid: string;
     targetNodeUuid: string;
-  },
-  {
+}, {
     groupId: string;
     sourceNodeUuid: string;
     targetNodeUuid: string;
     uuid?: string | undefined;
     createdAt?: Date | undefined;
-  }
->;
-export declare const CommunityEdgeSchema: z.ZodObject<
-  {
+}>;
+export declare const CommunityEdgeSchema: z.ZodObject<{
     uuid: z.ZodDefault<z.ZodString>;
     groupId: z.ZodString;
     sourceNodeUuid: z.ZodString;
     targetNodeUuid: z.ZodString;
     createdAt: z.ZodDefault<z.ZodDate>;
-  } & {
+} & {
     name: z.ZodString;
     description: z.ZodOptional<z.ZodString>;
-    factIds: z.ZodOptional<z.ZodArray<z.ZodString, 'many'>>;
-  },
-  'strip',
-  z.ZodTypeAny,
-  {
+    factIds: z.ZodOptional<z.ZodArray<z.ZodString, "many">>;
+}, "strip", z.ZodTypeAny, {
     uuid: string;
     name: string;
     groupId: string;
@@ -117,8 +95,7 @@ export declare const CommunityEdgeSchema: z.ZodObject<
     targetNodeUuid: string;
     factIds?: string[] | undefined;
     description?: string | undefined;
-  },
-  {
+}, {
     name: string;
     groupId: string;
     sourceNodeUuid: string;
@@ -127,37 +104,36 @@ export declare const CommunityEdgeSchema: z.ZodObject<
     createdAt?: Date | undefined;
     factIds?: string[] | undefined;
     description?: string | undefined;
-  }
->;
+}>;
 export declare abstract class Edge implements BaseEdge {
-  uuid: string;
-  groupId: string;
-  sourceNodeUuid: string;
-  targetNodeUuid: string;
-  createdAt: Date;
-  constructor(data: BaseEdge);
-  abstract save(driver: GraphDriver): Promise<void>;
-  delete(driver: GraphDriver): Promise<void>;
-  static deleteByUuids(driver: GraphDriver, uuids: string[]): Promise<void>;
-  static getByUuid(driver: GraphDriver, uuid: string): Promise<Edge | null>;
+    uuid: string;
+    groupId: string;
+    sourceNodeUuid: string;
+    targetNodeUuid: string;
+    createdAt: Date;
+    constructor(data: BaseEdge);
+    abstract save(driver: GraphDriver): Promise<void>;
+    delete(driver: GraphDriver): Promise<void>;
+    static deleteByUuids(driver: GraphDriver, uuids: string[]): Promise<void>;
+    static getByUuid(driver: GraphDriver, uuid: string): Promise<Edge | null>;
 }
 export declare class EntityEdgeImpl extends Edge implements EntityEdge {
-  name: string;
-  factIds: string[];
-  episodes: string[];
-  expiredAt?: Date;
-  validAt: Date;
-  invalidAt?: Date;
-  constructor(data: EntityEdge);
-  save(driver: GraphDriver): Promise<void>;
+    name: string;
+    factIds: string[];
+    episodes: string[];
+    expiredAt?: Date;
+    validAt: Date;
+    invalidAt?: Date;
+    constructor(data: EntityEdge);
+    save(driver: GraphDriver): Promise<void>;
 }
 export declare class EpisodicEdgeImpl extends Edge implements EpisodicEdge {
-  save(driver: GraphDriver): Promise<void>;
+    save(driver: GraphDriver): Promise<void>;
 }
 export declare class CommunityEdgeImpl extends Edge implements CommunityEdge {
-  name: string;
-  description?: string;
-  factIds?: string[];
-  constructor(data: CommunityEdge);
-  save(driver: GraphDriver): Promise<void>;
+    name: string;
+    description?: string;
+    factIds?: string[];
+    constructor(data: CommunityEdge);
+    save(driver: GraphDriver): Promise<void>;
 }
--- src/core/nodes.d.ts
+++ src/core/nodes.d.ts
@@ -1,54 +1,37 @@
 import { z } from 'zod';
-import {
-  BaseNode,
-  EntityNode,
-  EpisodicNode,
-  CommunityNode,
-  EpisodeType,
-  GraphDriver,
-} from '../types/index.js';
+import { BaseNode, EntityNode, EpisodicNode, CommunityNode, EpisodeType, GraphDriver } from '../types/index.js';
 export declare const EpisodeTypeSchema: z.ZodNativeEnum<typeof EpisodeType>;
-export declare const BaseNodeSchema: z.ZodObject<
-  {
+export declare const BaseNodeSchema: z.ZodObject<{
     uuid: z.ZodDefault<z.ZodString>;
     name: z.ZodString;
     groupId: z.ZodString;
-    labels: z.ZodDefault<z.ZodArray<z.ZodString, 'many'>>;
+    labels: z.ZodDefault<z.ZodArray<z.ZodString, "many">>;
     createdAt: z.ZodDefault<z.ZodDate>;
-  },
-  'strip',
-  z.ZodTypeAny,
-  {
+}, "strip", z.ZodTypeAny, {
     uuid: string;
     name: string;
     groupId: string;
     labels: string[];
     createdAt: Date;
-  },
-  {
+}, {
     name: string;
     groupId: string;
     uuid?: string | undefined;
     labels?: string[] | undefined;
     createdAt?: Date | undefined;
-  }
->;
-export declare const EntityNodeSchema: z.ZodObject<
-  {
+}>;
+export declare const EntityNodeSchema: z.ZodObject<{
     uuid: z.ZodDefault<z.ZodString>;
     name: z.ZodString;
     groupId: z.ZodString;
-    labels: z.ZodDefault<z.ZodArray<z.ZodString, 'many'>>;
+    labels: z.ZodDefault<z.ZodArray<z.ZodString, "many">>;
     createdAt: z.ZodDefault<z.ZodDate>;
-  } & {
+} & {
     entityType: z.ZodString;
     summary: z.ZodString;
-    summaryEmbedding: z.ZodOptional<z.ZodArray<z.ZodNumber, 'many'>>;
-    factIds: z.ZodOptional<z.ZodArray<z.ZodString, 'many'>>;
-  },
-  'strip',
-  z.ZodTypeAny,
-  {
+    summaryEmbedding: z.ZodOptional<z.ZodArray<z.ZodNumber, "many">>;
+    factIds: z.ZodOptional<z.ZodArray<z.ZodString, "many">>;
+}, "strip", z.ZodTypeAny, {
     uuid: string;
     name: string;
     groupId: string;
@@ -58,8 +41,7 @@ export declare const EntityNodeSchema: z.ZodObject<
     summary: string;
     summaryEmbedding?: number[] | undefined;
     factIds?: string[] | undefined;
-  },
-  {
+}, {
     name: string;
     groupId: string;
     entityType: string;
@@ -69,26 +51,21 @@ export declare const EntityNodeSchema: z.ZodObject<
     createdAt?: Date | undefined;
     summaryEmbedding?: number[] | undefined;
     factIds?: string[] | undefined;
-  }
->;
-export declare const EpisodicNodeSchema: z.ZodObject<
-  {
+}>;
+export declare const EpisodicNodeSchema: z.ZodObject<{
     uuid: z.ZodDefault<z.ZodString>;
     name: z.ZodString;
     groupId: z.ZodString;
-    labels: z.ZodDefault<z.ZodArray<z.ZodString, 'many'>>;
+    labels: z.ZodDefault<z.ZodArray<z.ZodString, "many">>;
     createdAt: z.ZodDefault<z.ZodDate>;
-  } & {
+} & {
     episodeType: z.ZodNativeEnum<typeof EpisodeType>;
     content: z.ZodString;
-    embedding: z.ZodOptional<z.ZodArray<z.ZodNumber, 'many'>>;
+    embedding: z.ZodOptional<z.ZodArray<z.ZodNumber, "many">>;
     validAt: z.ZodDate;
     invalidAt: z.ZodOptional<z.ZodDate>;
     referenceId: z.ZodOptional<z.ZodString>;
-  },
-  'strip',
-  z.ZodTypeAny,
-  {
+}, "strip", z.ZodTypeAny, {
     uuid: string;
     name: string;
     groupId: string;
@@ -100,8 +77,7 @@ export declare const EpisodicNodeSchema: z.ZodObject<
     embedding?: number[] | undefined;
     invalidAt?: Date | undefined;
     referenceId?: string | undefined;
-  },
-  {
+}, {
     name: string;
     groupId: string;
     episodeType: EpisodeType;
@@ -113,24 +89,19 @@ export declare const EpisodicNodeSchema: z.ZodObject<
     embedding?: number[] | undefined;
     invalidAt?: Date | undefined;
     referenceId?: string | undefined;
-  }
->;
-export declare const CommunityNodeSchema: z.ZodObject<
-  {
+}>;
+export declare const CommunityNodeSchema: z.ZodObject<{
     uuid: z.ZodDefault<z.ZodString>;
     name: z.ZodString;
     groupId: z.ZodString;
-    labels: z.ZodDefault<z.ZodArray<z.ZodString, 'many'>>;
+    labels: z.ZodDefault<z.ZodArray<z.ZodString, "many">>;
     createdAt: z.ZodDefault<z.ZodDate>;
-  } & {
+} & {
     communityLevel: z.ZodNumber;
     summary: z.ZodString;
-    summaryEmbedding: z.ZodOptional<z.ZodArray<z.ZodNumber, 'many'>>;
-    factIds: z.ZodOptional<z.ZodArray<z.ZodString, 'many'>>;
-  },
-  'strip',
-  z.ZodTypeAny,
-  {
+    summaryEmbedding: z.ZodOptional<z.ZodArray<z.ZodNumber, "many">>;
+    factIds: z.ZodOptional<z.ZodArray<z.ZodString, "many">>;
+}, "strip", z.ZodTypeAny, {
     uuid: string;
     name: string;
     groupId: string;
@@ -140,8 +111,7 @@ export declare const CommunityNodeSchema: z.ZodObject<
     communityLevel: number;
     summaryEmbedding?: number[] | undefined;
     factIds?: string[] | undefined;
-  },
-  {
+}, {
     name: string;
     groupId: string;
     summary: string;
@@ -151,42 +121,41 @@ export declare const CommunityNodeSchema: z.ZodObject<
     createdAt?: Date | undefined;
     summaryEmbedding?: number[] | undefined;
     factIds?: string[] | undefined;
-  }
->;
+}>;
 export declare abstract class Node implements BaseNode {
-  uuid: string;
-  name: string;
-  groupId: string;
-  labels: string[];
-  createdAt: Date;
-  constructor(data: BaseNode);
-  abstract save(driver: GraphDriver): Promise<void>;
-  delete(driver: GraphDriver): Promise<void>;
-  static getByUuid(driver: GraphDriver, uuid: string): Promise<Node | null>;
+    uuid: string;
+    name: string;
+    groupId: string;
+    labels: string[];
+    createdAt: Date;
+    constructor(data: BaseNode);
+    abstract save(driver: GraphDriver): Promise<void>;
+    delete(driver: GraphDriver): Promise<void>;
+    static getByUuid(driver: GraphDriver, uuid: string): Promise<Node | null>;
 }
 export declare class EntityNodeImpl extends Node implements EntityNode {
-  entityType: string;
-  summary: string;
-  summaryEmbedding?: number[];
-  factIds?: string[];
-  constructor(data: EntityNode);
-  save(driver: GraphDriver): Promise<void>;
+    entityType: string;
+    summary: string;
+    summaryEmbedding?: number[];
+    factIds?: string[];
+    constructor(data: EntityNode);
+    save(driver: GraphDriver): Promise<void>;
 }
 export declare class EpisodicNodeImpl extends Node implements EpisodicNode {
-  episodeType: EpisodeType;
-  content: string;
-  embedding?: number[];
-  validAt: Date;
-  invalidAt?: Date;
-  referenceId?: string;
-  constructor(data: EpisodicNode);
-  save(driver: GraphDriver): Promise<void>;
+    episodeType: EpisodeType;
+    content: string;
+    embedding?: number[];
+    validAt: Date;
+    invalidAt?: Date;
+    referenceId?: string;
+    constructor(data: EpisodicNode);
+    save(driver: GraphDriver): Promise<void>;
 }
 export declare class CommunityNodeImpl extends Node implements CommunityNode {
-  communityLevel: number;
-  summary: string;
-  summaryEmbedding?: number[];
-  factIds?: string[];
-  constructor(data: CommunityNode);
-  save(driver: GraphDriver): Promise<void>;
+    communityLevel: number;
+    summary: string;
+    summaryEmbedding?: number[];
+    factIds?: string[];
+    constructor(data: CommunityNode);
+    save(driver: GraphDriver): Promise<void>;
 }
